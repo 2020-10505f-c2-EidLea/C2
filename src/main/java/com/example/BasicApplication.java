1b8ca0c18db9ffdb2f9d6b85e7c21c62bcaea2e2
lea.eid@isae.edu.lb <>
2020-04-13 17:31:34 +0300

    This is the first commit

---
+++ src/main/java/com/example/BasicApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplication {
+
+	public static void main(String[] args) {
+            var ctx = SpringApplication.run(BasicApplication.class, args);
+            System.out.println("# Beans: " + ctx.getBeanDefinitionCount());
+                
+            var names = ctx.getBeanDefinitionNames();
+            Arrays.sort(names);
+            Arrays.asList(names).forEach(System.out::println);
+	}
+}
---
+++ src/main/java/com/example/MaClasse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Lea
+ */
+@Component
+public class MaClasse implements iClasse{
+
+    @Override
+    public void affiche() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
